14395231bf5e11d259cf0510b26cd46258a4b021
vllin <39091846+vllin@users.noreply.github.com>
2018-09-29 20:59:37 -0400

    Update suicides.R

--- suicides.R
+++ suicides.R
@@ -1,2 +1,2 @@
-cod <- read.csv(file = 'carolinadata-health/NCHS_-_Leading_Causes_of_Death__United_States.csv', header = TRUE, sep = ',')
+cod <- read.csv(file = 'NCHS_-_Leading_Causes_of_Death__United_States.csv', header = TRUE, sep = ',')
 suicides16 <- cod[(cod$Cause.Name == 'Suicide') & (nc_cod$Year == 2016),]
