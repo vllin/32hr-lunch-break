e0debad62e3762a7e63b4a44776a9ebeb618327d
Justin <justinmmott@gmail.com>
2018-09-30 07:18:47 -0400

    deleted og csvs

--- mammogram by state.py
+++ mammogram by state.py
@@ -1,25 +1,25 @@
 import csv
-with open('Asthma.csv') as filecsv:
+with open('Diagnosed chronic obstructive pulmonary .csv') as filecsv:
 	reader = csv.reader(filecsv, delimiter=',')
 	d={}
 	d2={}
 	i = 0
 	for row in reader:
-		if row[1] == 'DC' or row[2] == 'United States':
+		if row[2] == 'DC' or row[3] == 'United States' or row[3] == 'Guam' or row[3] == 'Virgin Islands' or row[3] == 'Puerto Rico' or row[10] == '':
 			continue
 		if i == 0:
 			i+=1
 			continue
 		check = False
 		for key in d.keys():
-			if key == row[2]:
+			if key == row[3]:
 				d2[key] += 1
-				d[key] += float(row[12])
+				d[key] += float(row[10])
 				check = True
 		if check == False:
-			d2[row[2]] = 1
-			d[row[2]] = float(row[12])
-	with open('Asthma by State.csv', 'w') as edi:
+			d2[row[3]] = 1
+			d[row[3]] = float(row[10])
+	with open('Diagnosed chronic obstructive pulmonary  by State.csv', 'w') as edi:
 		reader2 = csv.writer(edi)
 		for key in d.keys():
 			reader2.writerow([key, d[key]/d2[key]])
